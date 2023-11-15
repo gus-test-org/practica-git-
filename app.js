3a6d1460ce8c8aac66f0e26ddb54ace5e514f33a
gusbacoppel <134419517+gus-bancoppel@users.noreply.github.com>
2023-11-15 15:32:56 -0600

    Update app.js

--- app.js
+++ app.js
@@ -5,13 +5,5 @@ app.listen(port, function () {
 	console.log("Running port ", port);
 
 	console.log("Escribe una linea que imprima tu nombre abajo de esta.");
-	console.log("Lucia Guadalupe Angulo Flores");
 
-	console.log("Kevin Carmona Serrano")
-
-	console.log("Jose Andres Herrera Perez");
-	console.log("Nephtali Palillero Perez");
-
-	console.log("Lenin Meza")
-	console.log("Hector Manuel Muñoz Espinoza");
 })
