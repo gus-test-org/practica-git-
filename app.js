59fa7254bab81475de122cf87c16c4e36302cb2b
Lenin Meza <e_ljmeza@bancoppel.com>
2023-11-15 12:16:29 -0600

    :loud_sound: Log added

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const app = express();
 const port = 8080;
-app.listen(port, function(){
+app.listen(port, function () {
 	console.log("Running port ", port);
 
 	console.log("Escribe una linea que imprima tu nombre abajo de esta.");
-	console.log("Kevin Carmona Serrano")
-//=======
-	//console.log("Jose Andres Herrera Perez");
-//>>>>>>> da2d8e8e56c5a13b7eafb6210dfd3522e8d9fe4e
+	console.log("Lenin Meza")
 })
