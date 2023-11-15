55fa49fd5ab1b87b489ecdc48078b4a1f435cc1c
nepht-copp <nephtali.palillero@coppel.com>
2023-11-15 12:35:00 -0600

    agregar mi nombre

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ app.listen(port, function(){
 	console.log("Running port ", port);
 
 	console.log("Escribe una linea que imprima tu nombre abajo de esta.");
-	console.log("Kevin Carmona Serrano")
-	console.log("Nephtali Palillero Pérez")
+	console.log("Kevin Carmona Serrano");
+	console.log("Kevin Carmona Serrano");
 })
