1748abc4c4144eff8709b13d438d731b65dffca7
nemjoseah <jandresherrerap@outlook.com>
2023-11-15 12:34:31 -0600

    Update app.js
    Se descomenta mensaje de logueo en app.js

--- app.js
+++ app.js
@@ -7,4 +7,7 @@ app.listen(port, function () {
 	console.log("Escribe una linea que imprima tu nombre abajo de esta.");
 	console.log("Lucia Guadalupe Angulo Flores");
 
+	console.log("Kevin Carmona Serrano")
+
+	console.log("Jose Andres Herrera Perez");
 })
