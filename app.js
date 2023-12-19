bb5feee4fbf5aa482908028f76b02568da492946
Santiago Perea Castillo <spcneo@gmail.com>
2023-12-18 19:53:04 -0600

    Update app.js
    Signed-off-by: Santiago Perea Castillo <spcneo@gmail.com>

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
 const port = 8080;
+
+const name = "santiago";
+
 app.listen(port, function () {
 	console.log("Running port ", port);
-
+	console.log("Nombre: ", name);
 	console.log("Escribe una linea que imprima tu nombre abajo de esta.");
         OK
 })
